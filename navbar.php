41626044b9cf10050e2f6d7cda2f0b9b08bf8410
Ivanna Torres <itorres13@ucol.mx>
2024-04-25 02:29:21 -0600

    Diseño de login terminado

--- navbar.php
+++ navbar.php
@@ -9,8 +9,8 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <a class="nav-link active text-light" aria-current="page" href="ViewServicios.php">Servicios</a>
             </li>
@@ -20,18 +20,16 @@
             <li class="nav-item">
               <a class="nav-link active text-light" aria-current="page" href="ViewNosotros.php">Nosotros</a>
             </li>
-
-            
-            
           </ul>
+
           <div class="d-flex" role="text">
-            <button type="button" class="btn btn-success">
+            <a type="button" class="btn btn-success" href="Views/ViewLogin.php">
                 Iniciar 
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
                     <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
                 </svg>
-            </button>
+            </a>
           </div>
         </div>
       </div>
